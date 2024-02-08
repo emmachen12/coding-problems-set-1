bee37146e648770db58c0c3f122bed2c9523215a
emmachen12 <emc1207pi@gmail.com>
2024-02-08 14:55:49 -0500

    gjk

---
+++ example2.java
@@ -0,0 +1,13 @@
+public class example2
+{
+    public static void main (String[] args)
+    {
+    System.out.println("\n\n\t\t\t****************************************** ");
+    System.out.println("\n\n\t\t\t *\t                          \t*");
+    System.out.println("\n\n\t\t\t *\tSwimWithSue@swimwear.com\t*");
+    System.out.println("\n\n\t\t\t *\t                          \t*");
+
+    System.out.println("\n\n\t\t\t***************************************** ");
+
+    }
+}
---
+++ example3.java
@@ -0,0 +1,10 @@
+public class example3 {
+    public static void main (String[] args)
+    {
+    System.out.println(" Emma Chen");
+    System.out.println("\nSong: the sun will always be there wating after the rain ");
+    System.out.println("Proverb: You do not stay angry forever but delight to show mercy");
+
+    }
+}
+
